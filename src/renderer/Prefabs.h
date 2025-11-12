de3b05021f8d8d38ae19d5b128f5f65b7b4b8e1e
GRock3 <guyadar100@gmail.com>
2025-11-12 19:32:25 +0200

    added a sphere prefab

--- src/renderer/Prefabs.h
+++ src/renderer/Prefabs.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <cmath>
 #include "./Mesh.h"
 
 namespace Prefabs {
@@ -76,10 +77,53 @@ namespace Prefabs {
             2,3,4,
             3,0,4
         };
-        if(texturePath ==""){
-           return new Mesh(vertices, indices);
+        if (texturePath == "") {
+            return new Mesh(vertices, indices);
         }
         return new Mesh(vertices, indices, texturePath);
     }
 
-} // namespace Prefab
+    // UV sphere 
+    inline Mesh* Sphere(const std::string& texturePath = "", unsigned int sectorCount = 32, unsigned int stackCount = 16, float radius = 0.5f) {
+        std::vector<float> vertices;
+        std::vector<unsigned int> indices;
+
+        const float PI = 3.14159265359f;
+
+        for (unsigned int i = 0; i <= stackCount; ++i) {
+            float stackAngle = PI / 2 - i * (PI / stackCount); // from pi/2 to -pi/2
+            float xy = radius * cosf(stackAngle);
+            float z = radius * sinf(stackAngle);
+
+            for (unsigned int j = 0; j <= sectorCount; ++j) {
+                float sectorAngle = j * (2 * PI / sectorCount);
+
+                float x = xy * cosf(sectorAngle);
+                float y = xy * sinf(sectorAngle);
+                float nx = x / radius;
+                float ny = y / radius;
+                float nz = z / radius;
+                float s = (float)j / sectorCount;
+                float t = (float)i / stackCount;
+
+                vertices.insert(vertices.end(), { x, y, z, nx, ny, nz, s, t });
+            }
+        }
+
+        for (unsigned int i = 0; i < stackCount; ++i) {
+            unsigned int k1 = i * (sectorCount + 1);
+            unsigned int k2 = k1 + sectorCount + 1;
+
+            for (unsigned int j = 0; j < sectorCount; ++j, ++k1, ++k2) {
+                if (i != 0) indices.insert(indices.end(), { k1, k2, k1 + 1 });
+                if (i != (stackCount - 1)) indices.insert(indices.end(), { k1 + 1, k2, k2 + 1 });
+            }
+        }
+
+        if (texturePath == "") {
+            return new Mesh(vertices, indices);
+        }
+        return new Mesh(vertices, indices, texturePath);
+    }
+
+}
